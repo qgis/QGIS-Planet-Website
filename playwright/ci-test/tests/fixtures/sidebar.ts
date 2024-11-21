831ad5fc8b39d77a9098023bda125e4e22e7a04b
Lova Andriarimalala <43842786+Xpirix@users.noreply.github.com>
2024-11-21 15:21:40 +0300

    Add playwright e2e tests

---
+++ playwright/ci-test/tests/fixtures/sidebar.ts
@@ -0,0 +1,25 @@
+import type { Page, Locator } from "@playwright/test";
+
+export class Sidebar {
+    public readonly sidebar: Locator;
+    public readonly homeLink: Locator;
+    public readonly allPostsLink: Locator;
+    public readonly subscribersLink: Locator;
+    public readonly tagsLink: Locator;
+
+    constructor(public readonly page: Page) {
+        this.sidebar = this.page.locator("#sidebar");
+        this.homeLink = this.sidebar.getByRole("link", {
+            name: " Home",
+        });
+        this.allPostsLink = this.sidebar.getByRole("link", {
+            name: " All Posts",
+        });
+        this.subscribersLink = this.sidebar.locator('a')
+        .filter({ hasText: 'Subscribers' });
+
+        this.tagsLink = this.sidebar.getByRole("button", {
+            name: " Tags",
+        });
+    }
+}
